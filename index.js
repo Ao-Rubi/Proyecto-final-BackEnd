99977b9886dd2349b7fd9b006a66840787080bde
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-25 04:17:01 -0300

    se importaron los middlwares y se hizo su condifuracion correspondiente para conectarse a la red local

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Funciona")
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import * as dotenv from 'dotenv';
+
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'), () => {
+    console.log(`BD puerto : ${app.get('port')}`)
+})
+
+dotenv.config();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/burgers&beer');
