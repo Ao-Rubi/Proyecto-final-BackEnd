13965fc50097db2de56a71f3aa8b82a842bb8be0
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-26 14:59:12 -0300

    Agregado funcionamiento de creacion de usuario. Completar indicados.

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,9 +1,43 @@
+import Usuario from "../models/usuario"
+
 export const login = (req, res) => {
     res.send("logued")
 }
 
-export const crearUsuario = (req, res) => {
-    res.send("user creado")
+export const crearUsuario = async (req, res) => {
+
+    try {
+        const {nombre, email, password} = req.body;
+
+        // Verificar si el email existe
+        let usuario = await Usuario.findOne({email});
+        if (usuario) {
+            //User existe
+            return res.status(400).json({
+                mensaje: "Ya existe un usuario con este correo"
+            })
+        }
+
+        // Encriptar la contraseña
+
+        // Generar el token
+
+        // Guardamos el nuevo user en la Base de datos
+        usuario = new Usuario(req.body);
+        await usuario.save();
+        res.status(201).json({
+            mensaje: "Usuario creado correctamente",
+            nombre: usuario.nombre,
+            uid: usuario._id
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            mensaje: "El usuario no pudo ser creado"
+        })
+    }
+
 }
 
 // export const login = (res, req) => {
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,8 +3,13 @@ import { crearUsuario, login } from "../controllers/usuarios.controller";
 
 const router = Router();
 
-router.route("/usuario").post(login)
 
-router.route("/usuario/nuevo").post(crearUsuario)
+// Agregar validaciones con express validator
+router.route("/usuario")
+.get()
+.post(login);
+
+router.route("/usuario/nuevo")
+.post(crearUsuario);
 
 export default router;
