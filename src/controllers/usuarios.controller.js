ddfcb7e029a1d3f8702d0c4c25cf1b701c67b4a2
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-26 18:19:02 -0300

    logica para el encriptado de contraseña

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,4 +1,5 @@
-import Usuario from "../models/usuario"
+import Usuario from "../models/usuario";
+import bcrypt from "bcryptjs";
 
 export const login = (req, res) => {
     res.send("logued")
@@ -18,18 +19,23 @@ export const crearUsuario = async (req, res) => {
             })
         }
 
-        // Encriptar la contraseña
 
         // Generar el token
 
         // Guardamos el nuevo user en la Base de datos
         usuario = new Usuario(req.body);
+
+        // Encriptar la contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
         await usuario.save();
         res.status(201).json({
             mensaje: "Usuario creado correctamente",
             nombre: usuario.nombre,
             uid: usuario._id
         })
+
 
     } catch (error) {
         console.log(error)
@@ -40,9 +46,9 @@ export const crearUsuario = async (req, res) => {
 
 }
 
-// export const login = (res, req) => {
-//     res.send("logued")
-// }
+export const borrarUsuario = (res, req) => {
+    res.send("logued")
+}
 
 // export const login = (res, req) => {
 //     res.send("logued")
