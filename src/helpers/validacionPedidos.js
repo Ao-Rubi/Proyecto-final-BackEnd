980a25b3d6cdfa4234635c88ce1592a71494c7d2
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-27 19:50:34 -0300

    limpiamos comentarios y damos formato al documento

--- src/helpers/validacionPedidos.js
+++ src/helpers/validacionPedidos.js
@@ -10,21 +10,20 @@ const validarPedido = [
         .withMessage(
             'El nombre de usuario debe tener entre ( 4 y 30 ) caracteres'
         ),
-    //falta validar fecha,productosdelmenu y estado
-    //validar fecha
     check('fecha')
         .notEmpty()
         .withMessage('La fecha es obligatoria')
         .custom((input) => {
-            const result = moment(input, "DD/MM/YY",true).isValid();
-            console.log(result)
+            const result = moment(input, 'DD/MM/YY', true).isValid();
+            console.log(result);
             if (result) {
                 return true;
             } else {
-                throw new Error('La fecha debe respetar el siguiente formato ( DD/MM/AA )');
+                throw new Error(
+                    'La fecha debe respetar el siguiente formato ( DD/MM/AA )'
+                );
             }
         }),
-    //validar productos del menu
     check('productosdelmenu')
         .notEmpty()
         .withMessage('Los productos son obligatorios')
@@ -43,13 +42,13 @@ const validarPedido = [
         .withMessage(
             'Cada producto agregado debe contener entre (2 y 30 ) caracteres'
         ),
-        check('estado')
+    check('estado')
         .notEmpty()
         .withMessage('El estado es obligatorio')
-        .custom((input)=>{
-         if (input === Boolean(true)) return true;
-         else throw new Error( 'La respuesta debe ser de tipo booleana' );
-        }) ,
+        .custom((input) => {
+            if (input === Boolean(true)) return true;
+            else throw new Error('La respuesta debe ser de tipo booleana');
+        }),
     (req, res, next) => {
         resultadoValidacion(req, res, next);
     },
