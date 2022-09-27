2d85de39b3502c1710bf20bd4293c834b7b1f067
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-27 17:44:51 -0300

    validamos productos del menu

--- src/helpers/validacionPedidos.js
+++ src/helpers/validacionPedidos.js
@@ -10,5 +10,21 @@ const validarPedido = [
             'El nombre de usuario debe tener entre ( 4 y 30 ) caracteres'
         ),
     //falta validar fecha,productosdelmenu y estado
+    //validar productos del menu
+    check('productosdelmenu')
+        .custom((input) => {
+            let productosValidados = 0
+            input.forEach((producto) => {
+                if (producto.length >= 2 && producto.length <= 30) productosValidados++;
+            });
+            if (input.length === productosValidados) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .withMessage(
+            'Cada producto agregado debe contener entre (2 y 30 ) caracteres'
+        ),
 ];
 export default validarPedido;
