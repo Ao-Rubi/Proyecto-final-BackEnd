275cef56cfbdd23103fc539d2eee72f0aa5858ca
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-27 16:08:03 -0300

    Validaciones de login y creacion de usuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { crearUsuario, login } from "../controllers/usuarios.controller";
-import { check } from "express-validator";
+import { validarCreacionUsuario, validarUsuario } from "../helpers/validacionUsuario";
 
 const router = Router();
 
 
 // Agregar validaciones con express validator
 router.route("/usuario")
-.get()
-.post(login);
+.post(
+    [ validarUsuario ],login);
 
 router.route("/usuario/nuevo")
-.post([],crearUsuario);
+.post(
+    [ validarCreacionUsuario ],crearUsuario);
 
 export default router;
