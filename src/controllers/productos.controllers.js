fffc993cfda83b1968e9ef06d944cde4b5a25e7b
franAndrad <franciscoezequielandrade@gmail.com>
2022-09-27 16:52:57 -0300

    Revert "en crear producto se modifico para enviar directamente el req.body"
    This reverts commit 6b5ce07bf54825c51ad2544e32984b294a57dd95.

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -3,7 +3,14 @@ import Producto from "../models/producto";
 
 export const crearProducto = async (req,res) =>{
     try {
-        const nuevoProducto = new Producto(req.body);
+        const nuevoProducto = new Producto({
+            nombre: req.body.nombre,
+            estado: req.body.estado,
+            precio: req.body.precio,
+            detalle: req.body.detalle,
+            categoria: req.body.categoria,
+            imagen: req.body.imagen
+        });
         await nuevoProducto.save();
         res.status(201).json({
             mensaje: 'Producto creado'
