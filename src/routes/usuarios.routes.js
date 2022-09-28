0534bb916dfe1369b2c6f11856b2e986382f4f70
nicolas andrade <nicolasandrade328@gmail.com>
2022-09-27 23:53:34 -0300

    soluciono conflicto de import en la carpeta routes archvio usuarios.route.js

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,17 +1,17 @@
-import { Router } from "express";
-import { crearUsuario, login } from "../controllers/usuarios.controller";
-import { validarCreacionUsuario, validarUsuario } from "../helpers/validacionUsuario";
+import { Router } from 'express';
+import { crearUsuario, login } from '../controllers/usuarios.controller';
+import {
+    validarCreacionUsuario,
+    validarUsuario,
+} from '../helpers/validacionUsuario';
 
 const router = Router();
 
-
 // Agregar validaciones con express validator
-router.route("/")
-.post(
-    [ validarUsuario ],login);
+router.route('/')
+.post([validarUsuario], login);
 
-router.route("/nuevo")
-.post(
-    [ validarCreacionUsuario ],crearUsuario);
+router.route('/nuevo')
+.post([validarCreacionUsuario], crearUsuario);
 
 export default router;
