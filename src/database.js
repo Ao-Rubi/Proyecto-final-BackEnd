25c916cb65d215f8c78f0d5f1fe14cdc45261c9c
Garcia Mauro Nicolas <104022943+Ao-Rubi@users.noreply.github.com>
2022-10-02 13:26:32 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-// const url = 'mongodb://127.0.0.1:27017/burger&beer';
-const url = 'mongodb://localhost:27017/burgers&beers';
+// const url = 'mongodb://127.0.0.1:27017/burgersandbeerr';
+const url = 'mongodb://localhost:27017/burgersandbeer';
 
 mongoose.connect(url);
 
