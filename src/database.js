05b72dee8f098c529eb9127c3ec2b14aecc2777a
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-10-04 18:34:52 -0300

    cambio base de dato

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 // const url = 'mongodb://127.0.0.1:27017/burgersandbeerr';
-const url = 'mongodb://localhost:27017/burgersandbeer';
+// const url = 'mongodb://localhost:27017/burgersandbeer';
+const url = "mongodb+srv://Ao-Rubi:PHw1oZrxL4mHy8ry@cluster0.x3upctr.mongodb.net/burguersandbeer";
 
 mongoose.connect(url);
 
